e69d0ec5a14d09c376170fe45a38f5e0745011ee
travis.pola-carter <travis.pola-carter@spekit.net>
2020-03-17 12:51:17 +1100

    Better role descriptions

--- src/resources/role-descriptions.ts
+++ src/resources/role-descriptions.ts
@@ -3,8 +3,8 @@ import { ROLE_TYPES } from "app/crewmember/crewmember-names";
 
 export const ROLE_DESCRIPTIONS = new Map<ROLE_TYPES, string>();
 
-ROLE_DESCRIPTIONS.set(ROLE_TYPES.CAPTAIN, `Captain role is WIP`);
-ROLE_DESCRIPTIONS.set(ROLE_TYPES.NAVIGATOR, `Navigator role is WIP`);
-ROLE_DESCRIPTIONS.set(ROLE_TYPES.DOCTOR, `As the ${COL_TEAL}Doctor|r your main job is research Healthcare and splice Gene's into your fellow Crewmembers.`);
-ROLE_DESCRIPTIONS.set(ROLE_TYPES.MAJOR, ``);
+ROLE_DESCRIPTIONS.set(ROLE_TYPES.CAPTAIN, `The ${COL_GOLD}Captain|r controls station security targeting and pilots the Askellon through deep space.|nIt is your job to ensure your crew survive the trip.`);
+ROLE_DESCRIPTIONS.set(ROLE_TYPES.NAVIGATOR, `As ${COL_VENTS}Navigator|r you must scan deep space and lead your ${COL_GOLD}Captain|r through deep space.|nYou are also in charge of the Cargo Bay's ships.`);
+ROLE_DESCRIPTIONS.set(ROLE_TYPES.DOCTOR, `As ${COL_TEAL}Doctor|r research and upgrade Healthcare while using the Gene Splicer to upgrade your comrades.`);
+ROLE_DESCRIPTIONS.set(ROLE_TYPES.MAJOR, `Major is WIP`);
 ROLE_DESCRIPTIONS.set(ROLE_TYPES.SEC_GUARD, `${COL_ATTATCH}Security Guards|r gain 30% bonus experience when damaging Alien Hosts`);
