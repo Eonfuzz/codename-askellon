0d0cf35d2ad8c081de65e48ab823e5617688831e
maddeem <57919373+maddeem@users.noreply.github.com>
2021-03-17 00:59:39 -0700

    Updated logic to allow for invulnerable units to be detected

--- src/app/abilities/human/night-vision.ts
+++ src/app/abilities/human/night-vision.ts
@@ -4,10 +4,9 @@ import { VISION_TYPE } from "app/vision/vision-type";
 import { WorldEntity } from "app/world/world-entity";
 import { CrewFactory } from "app/crewmember/crewmember-factory";
 import { VisionFactory } from "app/vision/vision-factory";
-import { FilterAnyUnit } from "../../../resources/filters";
 import { SoundRef } from "app/types/sound-ref";
 import { SOUND_STR_SONIC_RES } from "resources/sounds";
-import { Vector2, vectorFromUnit } from "../../types/vector2";
+import { vectorFromUnit } from "../../types/vector2";
 
 const DURATION = 25;
 const RADIUS = 1200;
@@ -53,13 +52,18 @@ export class NightVisionAbility implements Ability {
             
             //Create a temporary group using the units within the effect radius
             let tempGroup = CreateGroup();
+            let filter = Filter(() => {
+                const fUnit = GetFilterUnit();
+                return !IsUnitVisible(fUnit,this.castingPlayer.handle) && UnitAlive(fUnit);
+            })
             GroupEnumUnitsInRange(
                     tempGroup, 
                     this.casterUnit.x, 
                     this.casterUnit.y, 
                     RADIUS, 
-                    FilterAnyUnit()
+                    filter
             );
+            DestroyFilter(filter);
             
             //Compare temporary group with previous temporary group and delete location data for units
             //no longer in range
@@ -77,17 +81,15 @@ export class NightVisionAbility implements Ability {
             //then update unit location data and add them to the checkMovementGroup
             ForGroup(tempGroup, () => {
                 const enumUnit: Unit = Unit.fromEnum();
-                if (!BlzIsUnitInvulnerable(enumUnit.handle) && !IsUnitVisible(enumUnit.handle,this.castingPlayer.handle)) {
-                    GroupAddUnit(this.checkMovementGroup,enumUnit.handle);
-                    let vec = vectorFromUnit(enumUnit.handle)
-                    if (this.locationData[enumUnit.id] != null && (this.locationData[enumUnit.id].x != vec.x || this.locationData[enumUnit.id].y != vec.y)){
-                        let tempDestructable = CreateDestructable(DESTRUCT_ID, enumUnit.x, enumUnit.y, 270, 1, 0);
-                        ShowDestructable(tempDestructable, this.locallyVisible);
-                        this.destructableList[this.destructableListMax] = tempDestructable;
-                        this.destructableListMax++;
+                GroupAddUnit(this.checkMovementGroup,enumUnit.handle);
+                let vec = vectorFromUnit(enumUnit.handle)
+                if (this.locationData[enumUnit.id] != null && (this.locationData[enumUnit.id].x != vec.x || this.locationData[enumUnit.id].y != vec.y)){
+                    let tempDestructable = CreateDestructable(DESTRUCT_ID, enumUnit.x, enumUnit.y, 270, 1, 0);
+                    ShowDestructable(tempDestructable, this.locallyVisible);
+                    this.destructableList[this.destructableListMax] = tempDestructable;
+                    this.destructableListMax++;
                     }
-                    this.locationData[enumUnit.id] = vec;
-                }
+                this.locationData[enumUnit.id] = vec;
                 return true;
             });
             DestroyGroup(tempGroup);
