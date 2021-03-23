05b4c2a4b5acdc750f6339ac23f8134ab8990040
maddeem <57919373+maddeem@users.noreply.github.com>
2021-03-22 20:37:02 -0700

    made new carrion behavior (WIP)

--- src/app/abilities/cult/carrion-reincarnation.ts
+++ src/app/abilities/cult/carrion-reincarnation.ts
@@ -1,5 +1,6 @@
 import { Unit } from "w3ts/index";
 import { ABILITY_HOOK } from "./hook-types";
+import {ABIL_CULTIST_DARK_THRALL} from "resources/ability-ids"
 
 export abstract class Behaviour {
 
@@ -16,18 +17,23 @@ export abstract class Behaviour {
         return true;
     }
 
+    /**
+     * Iterate upon this instance of the ability
+     */
     public onEvent(event: ABILITY_HOOK) {
-        if (event === ABILITY_HOOK.UnitDies) {
-            
+        if (event = ABILITY_HOOK.UnitDies) {
+            this.forUnit = Unit.fromHandle(GetTriggerUnit());
+            ReviveHero(this.forUnit.handle,this.forUnit.x,this.forUnit.y,false);
+            PauseUnit(this.forUnit.handle,true);
+            SetUnitAnimation(this.forUnit.handle,"death");
+            SetUnitInvulnerable(this.forUnit.handle,true);
         }
     }
 
-    public abstract onEvent(event: ABILITY_HOOK): void
+    public abstract step(deltaTime: number) {
 
-    /**
-     * Iterate upon this instance of the aiblity
-     */
-    public abstract step(deltaTime: number): void
+        return true;
+    }
 
     public doDestroy(): boolean {
         if (!this.forUnit.isAlive()) return true;
